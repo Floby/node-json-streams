f04b91de20ce1784618f5007d78012ebfd7cd5b9
Florent Jaby <florent.jaby@gmail.com>
2011-04-13 22:13:00 +0200

    add first test

---
+++ test/object.js
@@ -0,0 +1,26 @@
+var ParseStream = require('../lib/ParseStream');
+
+var json = JSON.stringify({
+    coucou: "salut",
+    number: 8,
+    bool: true,
+    meh: null,
+    nested_array: [
+        {
+            float: 1000.1,
+            string: "a long string ? \" coucou 'oui oui non non' \n\r"
+        },
+        5,
+        8,
+        "yet another string"
+    ]
+});
+console.log('parsing %s\n\n', json);
+
+var p = new ParseStream();
+p.on('end', function(o) {
+    console.log('result object', o);
+    console.log('ignored', this._ignored);
+    console.log('queue', this._queue);
+});
+p.end(json);
