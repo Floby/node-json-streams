e348811aabfb652eca928528f7ed3388a7bacdfb
Florent Jaby <florent.jaby@gmail.com>
2011-04-13 22:25:02 +0200

    use assert to test resulting object

--- test/object.js
+++ test/object.js
@@ -1,4 +1,5 @@
 var ParseStream = require('../lib/ParseStream');
+var assert = require('assert');
 
 var json = JSON.stringify({
     coucou: "salut",
@@ -8,7 +9,8 @@ var json = JSON.stringify({
     nested_array: [
         {
             float: 1000.1,
-            string: "a long string ? \" coucou 'oui oui non non' \n\r"
+            string: "a long string ? \" coucou 'oui oui non non' \n\r",
+            nothingmuch: null
         },
         5,
         8,
@@ -20,6 +22,8 @@ console.log('parsing %s\n\n', json);
 var p = new ParseStream();
 p.on('end', function(o) {
     console.log('result object', o);
+    var res = JSON.stringify(o);
+    assert.equal(res, json, "the objects are not identical");
     console.log('ignored', this._ignored);
     console.log('queue', this._queue);
 });
