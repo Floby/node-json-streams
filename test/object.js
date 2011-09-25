08a7178608e6b23954b22f65bb391758637c7fa4
Florent Jaby <florent.jaby@gmail.com>
2011-09-25 22:43:31 +0200

    display some JSONpath when setting values

--- test/object.js
+++ test/object.js
@@ -17,11 +17,23 @@ var json = JSON.stringify({
         "yet another string"
     ]
 });
+/*
+json = JSON.stringify([
+    'coucou',
+    'salut',
+    8,
+    true,
+    [
+        10,
+        false
+    ]
+]);
+*/
 console.log('parsing %s\n\n', json);
 
 var p = new ParseStream();
 p.on('end', function(o) {
-    console.log('result object', o);
+    console.log('\nresult object', o);
     var res = JSON.stringify(o);
     assert.equal(res, json, "the objects are not identical");
     console.log('ignored', this._ignored);
