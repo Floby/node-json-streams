ae3b54bf78dda4cae1c5c35d31174196da863102
Florent Jaby <florent.jaby@gmail.com>
2011-09-26 00:24:10 +0200

    forgot to return something in one setter

--- test/array.js
+++ test/array.js
@@ -1,22 +1,16 @@
 var ParseStream = require('../lib/ParseStream');
 var assert = require('assert');
 
-var json = JSON.stringify({
-    coucou: "salut",
-    number: 8,
-    bool: true,
-    meh: null,
-    nested_array: [
-        {
-            float: 1000.1,
-            string: "a long string ? \" coucou 'oui oui non non' \n\r",
-            nothingmuch: null
-        },
-        5,
-        8,
-        "yet another string"
+json = JSON.stringify([
+    'coucou',
+    'salut',
+    8,
+    true,
+    [
+        10,
+        false
     ]
-});
+]);
 console.log('parsing %s\n\n', json);
 
 var p = new ParseStream();
@@ -28,3 +22,4 @@ p.on('end', function(o) {
     console.log('queue', this._queue);
 });
 p.end(json);
+
