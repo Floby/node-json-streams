976607ef3b707e0374a01277f031f3325adce7b9
Florent Jaby <florent.jaby@gmail.com>
2012-08-05 14:56:42 +0200

    add some tests for numbers with vows. fix issue #9

---
+++ vows/single-value.vow.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+var vows = require('vows');
+var assert = require('assert');
+var ParseStream = require('../lib/ParseStream');
+
+
+var suite = vows.describe('Single Values');
+
+function assertSameParsed(value) {
+    function parseValue (value) {
+        return function() {
+            var parser = new ParseStream();
+            var cb = this.callback;
+            parser.on('end', function(object) {
+                cb(null, object);
+            });
+            parser.on('error', function(err) {
+                cb(err);
+            });
+            parser.end(value);
+        }
+    }
+    return {
+        topic: parseValue(value),
+        'it should be parsed correctly': function(parsed) {
+            parsed = JSON.stringify(parsed);
+            val = JSON.stringify(JSON.parse(value));
+            assert.equal(parsed, val,
+                'The parsed value is not equal to the given value '+parsed+' != '+val);
+        }
+    }
+}
+
+suite.addBatch({
+    "When parsing a number": {
+        "positive integer": assertSameParsed('4'),
+        "negative integer": assertSameParsed('-31'),
+        "positive fraction": assertSameParsed('10.89'),
+        "negative fraction": assertSameParsed('-40.54'),
+        "exponential notation": assertSameParsed('4e3'),
+        "exponential notation with decimal part": assertSameParsed('80.5e3'),
+        "exponential notation negative": assertSameParsed('-4e3'),
+        "exponential notation negative with decimal part": assertSameParsed('-4.6e3'),
+        "exponential notation, negative exponent": assertSameParsed('4e-3'),
+        "exponential notation, negative exponent, negative value": assertSameParsed('-4e-3'),
+        "exponential notation, negative exponent, negative value, decimal part": assertSameParsed('-4.8e-3'),
+    }
+}).export(module);
+
